65b89a575eb0bcd71ab5276bb9c253c84f87e3b5
Kardus Tenan <kardusanyar@yahoo.com>
2016-01-30 11:40:26 +0700

    coba lagi

--- app/src/main/java/com/afradadmedia/android/justjava/MainActivity.java
+++ app/src/main/java/com/afradadmedia/android/justjava/MainActivity.java
@@ -4,6 +4,7 @@ import android.os.*;
 import android.support.v7.app.*;
 import android.view.*;
 import android.widget.*;
+import java.text.*;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,13 +15,20 @@ public class MainActivity extends AppCompatActivity {
    }
    
    public void submitOrder(View view) {
-       display(1);
+       display(2);
+       displayPrice(2 * 5);
    }
    
    private void display(int number) {
        TextView quantityTextView = 
            (TextView) findViewById(R.id.quantity_text_view);
        quantityTextView.setText("" + number);
+   }
+   
+   private void displayPrice(int number) {
+       TextView priceTextView = 
+           (TextView) findViewById(R.id.price_text_view);
+       priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
    }
    
 }
